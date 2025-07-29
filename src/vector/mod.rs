1409ed84eb80f8a19477289ed0850ca661ae32a4
BOT #223 <2897544036@qq.com>
2025-07-29 11:30:35 +0800

    refactor: move type constraints to where clauses

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -21,13 +21,19 @@ pub struct Vector<Ty, const N: usize> {
     data: [Ty; N],
 }
 
-impl<Ty: Copy, const N: usize> Vector<Ty, N> {
+impl<Ty, const N: usize> Vector<Ty, N>
+where
+    Ty: Copy,
+{
     pub fn new(init_values: [Ty; N]) -> Self {
         Vector { data: init_values }
     }
 }
 
-impl<Ty: Default + Copy, const N: usize> Vector<Ty, N> {
+impl<Ty, const N: usize> Vector<Ty, N>
+where
+    Ty: Default + Copy,
+{
     pub fn zeros() -> Self {
         Vector {
             data: [Ty::default(); N],
@@ -35,7 +41,10 @@ impl<Ty: Default + Copy, const N: usize> Vector<Ty, N> {
     }
 }
 
-impl<Ty: Default + Copy + Mul<Output = Ty> + AddAssign, const N: usize> Vector<Ty, N> {
+impl<Ty, const N: usize> Vector<Ty, N>
+where
+    Ty: Default + Copy + Mul<Output = Ty> + AddAssign,
+{
     pub fn dot_product(lhs: &Vector<Ty, N>, rhs: &Vector<Ty, N>) -> Ty {
         let mut res = Ty::default();
 
@@ -47,7 +56,10 @@ impl<Ty: Default + Copy + Mul<Output = Ty> + AddAssign, const N: usize> Vector<T
     }
 }
 
-impl<Ty: Copy + Mul<Output = Ty> + Sub<Output = Ty>> Vector<Ty, 3> {
+impl<Ty> Vector<Ty, 3>
+where
+    Ty: Copy + Mul<Output = Ty> + Sub<Output = Ty>,
+{
     pub fn cross_product(lhs: &Vector<Ty, 3>, rhs: &Vector<Ty, 3>) -> Vector<Ty, 3> {
         Vector::new([
             lhs.data[1] * rhs.data[2] - lhs.data[2] * rhs.data[1],
@@ -57,7 +69,10 @@ impl<Ty: Copy + Mul<Output = Ty> + Sub<Output = Ty>> Vector<Ty, 3> {
     }
 }
 
-impl<Ty: Default + Copy + Mul<Output = Ty> + AddAssign + Sqrt, const N: usize> Vector<Ty, N> {
+impl<Ty, const N: usize> Vector<Ty, N>
+where
+    Ty: Default + Copy + Mul<Output = Ty> + AddAssign + Sqrt,
+{
     pub fn length(&self) -> Ty {
         let mut res = Ty::default();
 
@@ -69,8 +84,9 @@ impl<Ty: Default + Copy + Mul<Output = Ty> + AddAssign + Sqrt, const N: usize> V
     }
 }
 
-impl<Ty: Default + Copy + Mul<Output = Ty> + Div<Output = Ty> + AddAssign + Sqrt, const N: usize>
-    Vector<Ty, N>
+impl<Ty, const N: usize> Vector<Ty, N>
+where
+    Ty: Default + Copy + Mul<Output = Ty> + Div<Output = Ty> + AddAssign + Sqrt,
 {
     pub fn unit_vec(&self) -> Self {
         self / Vector::length(self) // may divide by zero
@@ -91,7 +107,10 @@ impl<Ty, const N: usize> IndexMut<usize> for Vector<Ty, N> {
     }
 }
 
-impl<Ty: Add<Output = Ty> + Default + Copy, const N: usize> Add<Self> for &Vector<Ty, N> {
+impl<Ty, const N: usize> Add<Self> for &Vector<Ty, N>
+where
+    Ty: Add<Output = Ty> + Default + Copy,
+{
     type Output = Vector<Ty, N>;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -105,7 +124,10 @@ impl<Ty: Add<Output = Ty> + Default + Copy, const N: usize> Add<Self> for &Vecto
     }
 }
 
-impl<Ty: Sub<Output = Ty> + Default + Copy, const N: usize> Sub<Self> for &Vector<Ty, N> {
+impl<Ty, const N: usize> Sub<Self> for &Vector<Ty, N>
+where
+    Ty: Sub<Output = Ty> + Default + Copy,
+{
     type Output = Vector<Ty, N>;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -119,7 +141,10 @@ impl<Ty: Sub<Output = Ty> + Default + Copy, const N: usize> Sub<Self> for &Vecto
     }
 }
 
-impl<Ty: Div<Output = Ty> + Default + Copy, const N: usize> Div<Ty> for &Vector<Ty, N> {
+impl<Ty, const N: usize> Div<Ty> for &Vector<Ty, N>
+where
+    Ty: Div<Output = Ty> + Default + Copy,
+{
     type Output = Vector<Ty, N>;
 
     fn div(self, rhs: Ty) -> Self::Output {
