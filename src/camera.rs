eca41169d7e85db6b03800fe7450c9808c303615
BOT #223 <2897544036@qq.com>
2025-07-29 18:38:54 +0800

    refactor: move render codes to Camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,6 @@
+use crate::ImageWriter;
+use crate::vector::Vector3d;
+
 pub struct Camera {
     image_width: usize,
     image_height: usize,
@@ -23,5 +26,22 @@ impl Camera {
             viewport_height: ((viewport_width as f64) / (image_height as f64)) as f64,
             focal_length,
         }
+    }
+
+    pub fn render(&self) {
+        let mut image_writer = ImageWriter::new(self.image_width, self.image_height);
+
+        for y in 0..self.image_height {
+            for x in 0..self.image_width {
+                let color = Vector3d::new([
+                    (x as f64) / (self.image_width as f64),
+                    (y as f64) / (self.image_height as f64),
+                    0.0,
+                ]);
+                image_writer.set_pixel_color_vec(x, y, &color);
+            }
+        }
+
+        image_writer.write_to_stdout();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,7 @@ mod vector;
 use image_writer::ImageWriter;
 use vector::*;
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
-
 fn main() {
-    let mut image_writer = ImageWriter::new(WIDTH, HEIGHT);
-
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            let color = Vector3d::new([
-                (x as f64) / (WIDTH as f64),
-                (y as f64) / (HEIGHT as f64),
-                0.0,
-            ]);
-            image_writer.set_pixel_color_vec(x, y, &color);
-        }
-    }
-
-    image_writer.write_to_stdout();
+    let main_camera = camera::Camera::new(100, 2.0, 16.0 / 9.0, 1.0);
+    main_camera.render();
 }
