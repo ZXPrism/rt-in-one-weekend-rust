b1b4db09006316109c08c8a4c99e9df9e34e0eeb
BOT #223 <2897544036@qq.com>
2025-07-30 09:56:05 +0800

    feat: simple raycasting

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use crate::ImageWriter;
+use crate::ray::{self, Ray};
 use crate::vector::Vector3d;
 
 pub struct Camera {
@@ -27,21 +28,34 @@ impl Camera {
             aspect_ratio,
             center: camera_center,
             viewport_width,
-            viewport_height: ((viewport_width as f64) / (image_height as f64)) as f64,
+            viewport_height: ((viewport_width as f64) / aspect_ratio) as f64,
             focal_length,
         }
     }
 
     pub fn render(&self) {
+        let viewport_u = Vector3d::new([self.viewport_width, 0.0, 0.0]);
+        let viewport_v = Vector3d::new([0.0, -self.viewport_height, 0.0]);
+        let viewport_u_delta = viewport_u / (self.image_width as f64);
+        let viewport_v_delta = viewport_v / (self.image_height as f64);
+        let viewport_upper_left = self.center + Vector3d::new([0.0, 0.0, self.focal_length])
+            - (viewport_u + viewport_v) / 2.0;
+        let pixel00 = viewport_upper_left - (viewport_u_delta + viewport_v_delta) / 2.0;
+
         let mut image_writer = ImageWriter::new(self.image_width, self.image_height);
 
         for y in 0..self.image_height {
             for x in 0..self.image_width {
-                let color = Vector3d::new([
-                    (x as f64) / (self.image_width as f64),
-                    (y as f64) / (self.image_height as f64),
-                    0.0,
-                ]);
+                let pixel = pixel00 + viewport_u_delta * (x as f64) + viewport_v_delta * (y as f64);
+                let ray_direction = pixel - self.center;
+                let ray_direction_norm = ray_direction.unit_vec();
+                let primary_ray = Ray::new(self.center, ray_direction);
+
+                let mut t = ray_direction_norm[1];
+                t = (t + 1.0) * 0.5;
+
+                let color =
+                    Vector3d::new([1.0, 1.0, 1.0]) * (1.0 - t) + Vector3d::new([0.5, 0.7, 1.0]) * t;
                 image_writer.set_pixel_color_vec(x, y, &color);
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ use vector::*;
 
 fn main() {
     let main_camera =
-        camera::Camera::new(100, 2.0, 16.0 / 9.0, Vector3d::new([1.0, 1.0, 1.0]), 1.0);
+        camera::Camera::new(400, 4.0, 16.0 / 9.0, Vector3d::new([1.0, 1.0, 1.0]), 1.0);
     main_camera.render();
 }
