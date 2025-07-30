deb0a7f128f458065fa1ecc8f52e91727c978e17
ZXPrism <2897544036@qq.com>
2025-07-30 08:26:15 +0800

    feat: add a new field `center` to `Camera`

--- src/camera.rs
+++ src/camera.rs
@@ -5,6 +5,8 @@ pub struct Camera {
     image_width: usize,
     image_height: usize,
     aspect_ratio: f64,
+
+    center: Vector3d,
     viewport_width: f64,
     viewport_height: f64,
     focal_length: f64,
@@ -15,6 +17,7 @@ impl Camera {
         image_width: usize,
         viewport_width: f64,
         aspect_ratio: f64,
+        camera_center: Vector3d,
         focal_length: f64,
     ) -> Self {
         let image_height = ((image_width as f64) / aspect_ratio) as usize;
@@ -22,6 +25,7 @@ impl Camera {
             image_width,
             image_height,
             aspect_ratio,
+            center: camera_center,
             viewport_width,
             viewport_height: ((viewport_width as f64) / (image_height as f64)) as f64,
             focal_length,
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use image_writer::ImageWriter;
 use vector::*;
 
 fn main() {
-    let main_camera = camera::Camera::new(100, 2.0, 16.0 / 9.0, 1.0);
+    let main_camera =
+        camera::Camera::new(100, 2.0, 16.0 / 9.0, Vector3d::new([1.0, 1.0, 1.0]), 1.0);
     main_camera.render();
 }
