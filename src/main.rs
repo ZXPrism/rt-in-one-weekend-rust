13094ca9124499996b70ff0bed9db5b3eb9359ca
BOT #223 <2897544036@qq.com>
2025-07-31 18:29:11 +0800

    fix: incorrect sphere color generation logic

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,17 @@ fn main() {
         1920,
         90.0,
         16.0 / 9.0,
-        Vector3d::new([0.0, 1.5, -1.5]),
-        Vector3d::new([0.0, 0.4, -0.2]),
-        32,
-        32,
+        Vector3d::new([0.0, 1.5, -1.3]),
+        Vector3d::new([0.0, 0.4, 0.0]),
+        256,
+        64,
         0.0,
         1.0,
     );
 
     let mut main_scene = Scene::new();
 
-    let diffuse_ground = Box::new(DiffuseMaterial::new(Color::new([0.5, 0.5, 0.5])));
+    let diffuse_ground = Box::new(DiffuseMaterial::new(Color::new([0.3, 0.3, 0.3])));
 
     main_scene.add_object(Box::new(Sphere::new(
         Vector3d::new([0.0, -1000.0, 1.0]),
@@ -46,16 +46,34 @@ fn main() {
 
     for i in -3..5 {
         for j in 0..5 {
+            let mut random_color = Color::random_unit_sphere();
+
+            // correct negative elements
+            random_color[0] = random_color[0].abs();
+            random_color[1] = random_color[1].abs();
+            random_color[2] = random_color[2].abs();
+
+            // prevent too dark colors
+            if random_color[0] < 0.2 {
+                random_color[0] += 0.2;
+            }
+            if random_color[1] < 0.2 {
+                random_color[1] += 0.2;
+            }
+            if random_color[2] < 0.2 {
+                random_color[2] += 0.2;
+            }
+
             let material_choice = rng.random_range(0..10);
             let material: Box<dyn Material> = if material_choice <= 5 {
-                Box::new(DiffuseMaterial::new(Color::random_unit_sphere()))
+                Box::new(DiffuseMaterial::new(random_color))
             } else if material_choice <= 7 {
-                Box::new(MetalMaterial::new(Color::random_unit_sphere(), 0.0))
+                Box::new(MetalMaterial::new(random_color, 0.0))
             } else {
                 Box::new(DielectricMaterial::new(1.5))
             };
 
-            let random_radius = rng.random_range(0.1..0.3);
+            let random_radius = rng.random_range(0.1..0.35);
 
             main_scene.add_object(Box::new(Sphere::new(
                 Vector3d::new([
