2b50ad23b7fccdea0d20dd135c53c6aed536ad74
BOT #223 <2897544036@qq.com>
2025-07-31 11:17:11 +0800

    refactor: move random unit vector generation logic to a new associated function of `Vector`

--- src/scene/drawable/sphere.rs
+++ src/scene/drawable/sphere.rs
@@ -48,9 +48,8 @@ impl Drawable for Sphere {
     }
 
     fn fill_info(&self, ray: &Ray, hit_info: &mut HitInfo) {
-        hit_info.if_hit = true;
         hit_info.normal = ray.at(hit_info.t) - self.center;
         hit_info.normal /= self.radius;
-        self.material.scatter(ray, hit_info);
+        hit_info.if_hit = self.material.scatter(ray, hit_info);
     }
 }
